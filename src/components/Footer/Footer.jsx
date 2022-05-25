d394cb7439443598e3316e28ec7c88c0ebc6abc4
GodDorae <dohyun6425@naver.com>
2022-05-25 16:08:55 +0900

    Initial footer commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import classes from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <div className={classes.container}>
+            <div>"It ain't over till it's over"</div>
+            <div>Copyright Dohyun Lee 2022</div>
+            <div>dohyun6425@naver.com</div>
+        </div>
+    );
+}
+
+export default Footer;
