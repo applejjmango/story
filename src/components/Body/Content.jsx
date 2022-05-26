d77ec64ff0f47b7691909b06732b8f8b73751941
GodDorae <dohyun6425@naver.com>
2022-05-26 22:58:41 +0900

    Make media query (PC, Tablet. Mobile yet)

--- src/components/Body/Content.jsx
+++ src/components/Body/Content.jsx
@@ -1,17 +1,22 @@
-import { Link } from 'react-router-dom';
-import classes from './Content.module.css';
+import { Link } from "react-router-dom";
+import classes from "./Content.module.css";
 
 function Content() {
-    return(
-        <div className={classes.container}>
-            <div className={classes.slogan}>
-                Let's create<br/>
-                something<br/>
-                new.
-            </div>
-            <button className={classes.info}><Link to="short-page" className={classes.sentence}>More information about Lotte Giants</Link></button>
-        </div>
-    )
+  return (
+    <div className={classes.container}>
+      <div className={classes.slogan}>
+        Let's create{window.innerWidth > 1450 ? <br /> : " "}
+        something{window.innerWidth > 1450 ? <br /> : " "}
+        new.
+      </div>
+      <button className={classes.info}>
+        <Link to="short-page" className={classes.sentence}>
+          More information {window.innerWidth > 850 ? "" : <br />}about Lotte
+          Giants
+        </Link>
+      </button>
+    </div>
+  );
 }
 
 export default Content;
