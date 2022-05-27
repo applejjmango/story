c053318766f28eca2f9fbfd706c2a82eb9f47f84
GodDorae <dohyun6425@naver.com>
2022-05-27 13:02:42 +0900

    Restore

--- src/components/Short-page/UI/TextContainer.jsx
+++
@@ -1,7 +0,0 @@
-import "./TextContainer.css";
-
-function TextContainer(props) {
-  return <div className="textcontainer" data-index={props.number}>{props.children}</div>;
-}
-
-export default TextContainer;
