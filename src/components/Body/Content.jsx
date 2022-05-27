6dc45a320cc535f8b9d863079f20d283bef8bdf3
GodDorae <dohyun6425@naver.com>
2022-05-27 09:57:25 +0900

    Adapt react-responsive

--- src/components/Body/Content.jsx
+++ src/components/Body/Content.jsx
@@ -1,17 +1,26 @@
+import { useMediaQuery } from "react-responsive";
 import { Link } from "react-router-dom";
 import classes from "./Content.module.css";
 
 function Content() {
+  const isBigMonitor = useMediaQuery({
+    query: "(min-width:1450px)",
+  });
+
+  const isTablet = useMediaQuery({
+    query: "(max-width:850px)",
+  });
+
   return (
     <div className={classes.container}>
       <div className={classes.slogan}>
-        Let's create{window.innerWidth > 1450 ? <br /> : " "}
-        something{window.innerWidth > 1450 ? <br /> : " "}
+        Let's create{isBigMonitor ? <br /> : " "}
+        something{isBigMonitor ? <br /> : " "}
         new.
       </div>
       <button className={classes.info}>
         <Link to="short-page" className={classes.sentence}>
-          More information {window.innerWidth > 850 ? null : <br />}about Lotte
+          More information {isTablet ? <br /> : null}about Lotte
           Giants
         </Link>
       </button>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
+import { useMediaQuery } from "react-responsive";
 import classes from "./Footer.module.css";
 
 function Footer() {
+    const isMobile = useMediaQuery({
+        query: "(max-width:670px)"
+    });
+
     return (
         <div className={classes.container}>
-            <div>"It ain't over{window.innerWidth > 670 ? null : <br />} till it's over"</div>
-            <div>Copyright{window.innerWidth > 670 ? null : <br />} Dohyun Lee 2022</div>
+            <div>"It ain't over{isMobile && <br/>} till it's over"</div>
+            <div>Copyright{isMobile && <br/>} Dohyun Lee 2022</div>
             <div>dohyun6425@naver.com</div>
         </div>
     );
