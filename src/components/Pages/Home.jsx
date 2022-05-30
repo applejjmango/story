f274a4b5569e4baf1046de3d347c64af556461c7
applejjmango <englishtour0224@gmail.com>
2022-05-30 16:48:30 +0900

    updated

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,16 +1,15 @@
-import './Home.css';
-import Header from '../Header/Header';
-import Body from '../Body/Body';
-import Footer from '../Footer/Footer';
+import "./Home.css";
+import Header from "../Header/Header";
+import Body from "../Body/Body";
+import Footer from "../Footer/Footer";
 
 function Home() {
   return (
     <div className="Home">
       <Header />
       <Body />
-      <Footer />
     </div>
-  )
+  );
 }
 
 export default Home;
